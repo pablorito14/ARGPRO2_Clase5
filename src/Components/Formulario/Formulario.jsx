4889a22c023e6930c04ad404d8a93b37333f684b
Pablo Rito <pablorito14@gmail.com>
2023-11-14 21:42:08 -0300

    clase terminada

--- src/Components/Formulario/Formulario.jsx
+++ src/Components/Formulario/Formulario.jsx
@@ -7,6 +7,8 @@ const Formulario = ({}) => {
 
   const [mostrarValores,setMostrarValores] = useState(false);
 
+  const [mostrarPassword,setMostrarPassword] = useState(false);
+
   const actualizarNombre = (e) => {
     console.log(e.target.value)
     setNombre(e.target.value);
@@ -27,27 +29,43 @@ const Formulario = ({}) => {
     setMostrarValores(true);
   }
 
+  const actualizarEstado = (e) => {
+    e.preventDefault();
+    setMostrarPassword(!mostrarPassword)
+  }
+
   return(
     <div>
       <div className="row my-5 justify-content-md-center">
-        <div className="col-md-5">
+        <div className="col-md-7">
           <h3>NO SE USA</h3>
-          <form novalidate>
+          <form>
             <div className="form-group">
               <label htmlFor="">Nombre</label>
               <input type="text" className='form-control' placeholder='Ingrese nombre' 
                       value={nombre} onChange={actualizarNombre}/>
             </div>
+
             <div className="form-group">
               <label htmlFor="">Email</label>
               <input type="email" className='form-control' placeholder='Ingrese email' 
                       value={email} onChange={actualizarEmail}/>
             </div>
+
+            <div className="form-row d-flex">
+              <div className="col-md-7">
+                <input type={mostrarPassword ? 'text' : 'password'} className='form-control' />
+              </div>
+              <button className='btn btn-danger w-100' onClick={actualizarEstado}>{mostrarPassword ? 'Ocultar' : 'Mostrar'} password</button>
+            </div>
+
             <div className="form-group">
               <label htmlFor="">Dirección</label>
               <input type="text" className='form-control' placeholder='Ingrese dirección' 
                       value={direccion} onChange={actualizarDireccion}/>
             </div>
+
+
             <button className='btn btn-success'
                     type='submit' onClick={enviarDatos}>Enviar</button>
           </form>
